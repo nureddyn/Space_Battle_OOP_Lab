ffb0d632f18ac24560cb4454abac1bb28941c94e
nureddyn <nureddynballout@gmail.com>
2023-11-03 11:01:00 -0300

    Classes improved

--- script.js
+++ script.js
@@ -1,63 +1,66 @@
 let Rounds = [];
 
+// General actions and results is going to be made here
 class Round {
     constructor(Array) {
         this.array = Array;
     }
 }
 
-// class GameObject {
-//     constructor(hull, firePower, accuracy) {
-//         this.hull = hull;
-//         this.firePower = firePower;
-//         this.accuracy = accuracy;
-//     }
-// }
-
-class Ship {
-    hull = 20;
-    firePower = 5;
-    accuracy = 0.7; 
-
-    displayName () {
-        console.log("I'm a Ship");
+class Character {
+    constructor(type, hull, firePower, accuracy) {
+        this.type = type;
+        this.hull = hull;
+        this.firePower = firePower;
+        this.accuracy = accuracy;
     }
-    attack(alien) {
-        alien.hull-=this.firePower;
+    attack(target) {
+        // This depends on accuracy
+        if (Math.random() < this.accuracy) {
+            target.hull-=this.firePower;
+    }
     }
     getProperties () {
-        return "Ship: ", {
+        return {
+            'type': this.type,
             "hull": this.hull,
             "firePower": this.firePower,
             "accuracy": this.accuracy
         }
+    }
+    move() {
+        return 'I am moving';
+    }
+    displayName () {
+        return `I am a ${this.type}`;
     }
 }
 
-class Alien {
-    hull = Math.floor(Math.random() * (6 - 3) + 3);
-    firePower = Math.floor(Math.random() * (4 - 2) + 2);
-    accuracy = Math.random() * (0.8 - 0.6) + 0.6;
-
-    displayName () {
-        console.log("I'm an Alien");
+class Ship extends Character {
+    constructor(type, hull, firePower, accuracy) {
+        super(type, hull, firePower, accuracy)
+        this.type = 'Ship';
+        this.hull = 20;
+        this.firePower = 5;
+        this.accuracy = 0.7;
     }
-    getProperties () {
-        return "Alien: ", {
-            "hull": this.hull,
-            "firePower": this.firePower,
-            "accuracy": this.accuracy
-        }
+}
+
+class Alien extends Character {
+    constructor(type, hull, firePower, accuracy) {
+        super(type, hull, firePower, accuracy)
+        this.type = 'Alien';
+        this.hull = Math.floor(Math.random() * (6 - 3) + 3);
+        this.firePower = Math.floor(Math.random() * (4 - 2) + 2);
+        this.accuracy = Number((Math.random() * (0.8 - 0.6) + 0.6).toFixed(2));
     }
 }
 
 let ship1 = new Ship()
 // console.log(ship1);
 let alien1 = new Alien();
-// console.log(alien1);
+console.log(alien1.displayName());
+console.log(ship1.displayName());
 let firstRound = new Round([ship1.getProperties(), alien1.getProperties()]);
 
-// console.log(firstRound);
-ship1.attack(alien1);
-
-// console.log(firstRound);
+// ship1.attack(alien1);
